4e3136d871ae3e75c09297191880eceac226b2c5
Juha Ruistola <juha.ruistola@metaplay.io>
2025-04-02 18:13:47 +0300

    Removed the DIY AST node Stringer interface implementations (because godump)

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,7 @@
 package ast
 
 import (
-	"fmt"
 	"jru-test/lexer"
-	"strings"
 )
 
 type Type interface {
@@ -24,19 +22,11 @@ type SymbolType struct {
 
 func (n SymbolType) _type() {}
 
-func (n SymbolType) String() string {
-	return fmt.Sprintf("(Type %s)", n.TypeName)
-}
-
 type ArrayType struct {
 	UnderlyingType Type
 }
 
 func (n ArrayType) _type() {}
-
-func (n ArrayType) String() string {
-	return fmt.Sprintf("(Type %s[])", n.UnderlyingType)
-}
 
 type StringExpr struct {
 	Value string
@@ -44,19 +34,11 @@ type StringExpr struct {
 
 func (n StringExpr) expr() {}
 
-func (n StringExpr) String() string {
-	return fmt.Sprintf("(String literal \"%s\")", n.Value)
-}
-
 type SymbolExpr struct {
 	Value string
 }
 
 func (n SymbolExpr) expr() {}
-
-func (n SymbolExpr) String() string {
-	return fmt.Sprintf("(Symbol \"%s\")", n.Value)
-}
 
 type NumberExpr struct {
 	Value float64
@@ -64,20 +46,12 @@ type NumberExpr struct {
 
 func (n NumberExpr) expr() {}
 
-func (n NumberExpr) String() string {
-	return fmt.Sprintf("(Number %v)", n.Value)
-}
-
 type UnaryExpr struct {
 	Operator lexer.Token
 	Rhs      Expr
 }
 
 func (n UnaryExpr) expr() {}
-
-func (n UnaryExpr) String() string {
-	return fmt.Sprintf("(%s%s)", n.Operator.Value, n.Rhs)
-}
 
 type BinaryExpr struct {
 	Lhs      Expr
@@ -87,25 +61,11 @@ type BinaryExpr struct {
 
 func (n BinaryExpr) expr() {}
 
-func (n BinaryExpr) String() string {
-	return fmt.Sprintf("(%s %s %s)", n.Operator.Value, n.Lhs, n.Rhs)
-}
-
 type BlockStmt struct {
 	Body []Stmt
 }
 
 func (n BlockStmt) stmt() {}
-
-func (n BlockStmt) String() string {
-	var sb strings.Builder
-	sb.WriteString("(")
-	for _, expr := range n.Body {
-		sb.WriteString(fmt.Sprintf("(Stmt %s) ", expr))
-	}
-	sb.WriteString(")")
-	return fmt.Sprintf(sb.String())
-}
 
 type ExpressionStmt struct {
 	Expr Expr
@@ -113,26 +73,12 @@ type ExpressionStmt struct {
 
 func (n ExpressionStmt) stmt() {}
 
-func (n ExpressionStmt) String() string {
-	return fmt.Sprintf("(ExprStmt %s)", n.Expr)
-}
-
 type VarDeclStmt struct {
 	Var     TypedIdent
 	InitVal Expr
 }
 
 func (n VarDeclStmt) stmt() {}
-
-func (n VarDeclStmt) String() string {
-	var initVal string
-	if n.InitVal != nil {
-		initVal = fmt.Sprintf("%s", n.InitVal)
-	} else {
-		initVal = "none"
-	}
-	return fmt.Sprintf("(Var name %s type %s initial value %s)", n.Var.Name, n.Var.Type, initVal)
-}
 
 type TypedIdent struct {
 	Name string
@@ -148,34 +94,14 @@ type FuncDeclStmt struct {
 
 func (n FuncDeclStmt) stmt() {}
 
-func (n FuncDeclStmt) String() string {
-	var returnType string
-	if n.ReturnType != nil {
-		returnType = fmt.Sprintf("%s", n.ReturnType)
-	} else {
-		returnType = "none"
-	}
-	return fmt.Sprintf("(Func name %s params (%s) returntype %s body { %s })", n.Name, n.Parameters, returnType, n.Body)
-}
-
 type IfStmt struct {
 	// TODO
 }
 
 func (n IfStmt) stmt() {}
 
-func (n IfStmt) String() string {
-	// TODO
-	return ""
-}
-
 type ForStmt struct {
 	// TODO
 }
 
 func (n ForStmt) stmt() {}
-
-func (n ForStmt) String() string {
-	// TODO
-	return ""
-}
